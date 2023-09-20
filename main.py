7e096483cbe6422725dd0e78bab6ba43721ae93d
Selimbey Simsek <selimbey.simsek@outlook.de>
2023-09-20 18:25:51 +0200

    All Plug-Ins installed

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello World!")
-print("For now everything is working great!")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "King Selimbey"}
+
+@app.get("/items/{item_id}")
+async def get_item(item_id):
+    return{"item_id": item_id}
